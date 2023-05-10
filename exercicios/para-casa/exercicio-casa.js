a71f72626b374103d68540d61136cf425fd7628a
Jamile <milli.santana2017@gmail.com>
2023-05-10 17:01:14 -0300

    Feat: Adiciona exercicio da semana 7

--- exercicios/para-casa/exercicio-casa.js
+++ exercicios/para-casa/exercicio-casa.js
@@ -1,59 +1,46 @@
-const livros = require("./biblioteca.json")
-const usuarios = require("./usuarios.json")
+const livros = require("./biblioteca.json");
+const usuarios = require("./usuarios.json");
 
 const buscaLivroPorId = (id) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const livroEncontrado = livros.find(livro => livro.id === id)
       if (livroEncontrado) {
-        resolve(livroEncontrado)
+        resolve(livroEncontrado);
       } else {
-        reject(`Livro não foi encontrado`)
+        reject(`Livro não foi encontrado`);
       }
-    }, 5000)
-  })
+    }, 2000);
+  });
 }
 
-console.log("Buscador de livros")
-
-const idLivroBuscado = 123
+console.log("Buscador de livros");
+const idLivroBuscado = 124;
+// const idLivroBuscado = 1245;
+// const idLivroBuscado = 123;
 
 buscaLivroPorId(idLivroBuscado)
   .then((livro) => {
-    console.log(`Título: ${livro.nome}`)
-    console.log(`Autor: ${livro.autor}`)
-  })
-  .catch((erro) => {
-    console.log(erro)
-  })
-
-console.log(`Solicitada a busca para o livro id #${idLivroBuscado}`)
-
-const buscaUsuariosPorLivro = (livroId) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const usuarioEncontrados = usuarios.find(usuario => usuario.id === livroId)
-      if (usuarioEncontrados) {
-        resolve(usuarioEncontrados)
-      } else {
-        reject(`Usuario não foi encontrado`)
-      }
-      resolve(usuarioEncontrados);
-    }, 5000);
-  });
-};
-
-buscaUsuariosPorLivro()
-  .then((usuarios) => {
-    console.log("---");
-    console.log("Usuários:");
-    usuarios.forEach((usuario) => {
-      console.log(`\nNome: ${usuario.nome}`);
-      console.log(`Email: ${usuario.email}`);
-    });
+    console.log(`Título: ${livro.nome}`);
+    console.log(`Autor: ${livro.autor}`);
+    console.log("-----");
+    console.log("Usuários:\n");
+    buscaUsuario(livro);
   })
   .catch((erro) => {
     console.log(erro);
   });
 
-  console.log(`Solicitada a busca para o usuario id #${idLivroBuscado}`)
+console.log(`Solicitada a busca para o livro id #${idLivroBuscado}\n`);
+
+const buscaUsuario = async (livroBuscado) => {
+  await new Promise(() => {
+    setTimeout(() => {
+      livroBuscado.usuarios.forEach((elemento) => {
+        const usuarioEncontrado = usuarios.find((usuario) => usuario.id === elemento);
+        console.log(`Nome: ${usuarioEncontrado.nome}`);
+        console.log(`Email: ${usuarioEncontrado.email}\n`);
+      });
+    }, 2000);
+  });
+}
